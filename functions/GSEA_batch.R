f963086f94d32d73203283dd82a46f4aea7b00ca
Yonglin Lu <lyonglin@seas.upenn.edu>
2023-07-25 16:28:21 -0400

    Update GSEA_batch.R

--- functions/GSEA_batch.R
+++ functions/GSEA_batch.R
@@ -19,7 +19,7 @@ GSEA_batch <- function(
   save_dirs <- lapply(rnks, function(x) dir_create(out_dir,strsplit(x,'/')[[1]][2],''))
   rnks <- paste0(getwd(),'\\\\',rnks)
   
-  batch <- function(rnk=rnks[4],gene_set=gene_sets[4],save_dir=save_dirs[4],name){
+  batch <- function(rnk=rnks[1],gene_set=gene_sets[4],save_dir=save_dirs[1],name){
     
     rnk <- gsub('/','\\\\',rnk,fixed = TRUE)
     
@@ -37,6 +37,7 @@ GSEA_batch <- function(
     command <- gsub("_gene_set_", gene_set, command)
     command <- gsub("_save_dir_", save_dir, command)
     command <- gsub("_the_label_", paste0(basename(save_dir),'--',gsub(' ','_',name)), command,fixed = T)
+    command <- gsub('_cd_',GSEA_path, command)
     print(paste0('Performing enrichment analysis of ---  ',basename(rnk),'  --- with ---  ' ,name,' : ',gene_set,' .....' ))
     system("cmd.exe",input=command,show.output.on.console = F)
     Sys.sleep(1)
