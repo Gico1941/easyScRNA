339703842a5cc8be9f2bbf72dfac4b1ccbbd1337
Yonglin Lu <lyonglin@seas.upenn.edu>
2024-01-19 01:46:32 -0500

    Delete functions directory

--- functions/Call_info.R
+++
@@ -1,5 +0,0 @@
-####################################### call info
-call_info <- function(data){
-  cat(paste0('\n\n-----------------------------------useful info for find DEG--------------------------------- :\n' ,
-             'groups in dataset :',paste0(unique(data$group),collapse = ','),'\n','clusters in dataset :',paste0(levels(data$seurat_clusters ),collapse = ' ')))
-}
--- functions/GSEA_batch.bat
+++
@@ -1,2 +0,0 @@
-cd GSEA_4.3.2
-gsea-cli.bat GSEAPreranked -gmx ftp.broadinstitute.org://pub/gsea/msigdb/mouse/gene_sets/_gene_set_ -collapse Remap_Only -mode Abs_max_of_probes -norm meandiv -nperm 1000 -rnd_seed timestamp -rnk _rnk_file_ -scoring_scheme weighted -rpt_label _the_label_ -chip ftp.broadinstitute.org://pub/gsea/msigdb/mouse/annotations/_chip_ -create_svgs false -include_only_symbols true -make_sets true -plot_top_x _numberplot_ -set_max 500 -set_min 15 -zip_report false -out _save_dir_
